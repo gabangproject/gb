227b1e5d5dbe2215262656f8ba4af07dc73dbf60
han <sist59@sist>
2018-05-18 14:01:11 +0900

    한 : 매물 목록 수정 중

--- src/com/gabang/model/MaemoolModel.java
+++ src/com/gabang/model/MaemoolModel.java
@@ -1,16 +1,21 @@
 package com.gabang.model;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import com.gabang.controller.Controller;
 import com.gabang.controller.RequestMapping;
+import com.gabang.vo.PropertyAddrDAO;
+import com.gabang.vo.PropertyAddrVO;
 
 @Controller
 public class MaemoolModel{
 	@RequestMapping("main/maemool_list.do")
 	public String maemoolList(HttpServletRequest req) {
+		List<PropertyAddrVO> geoList = PropertyAddrDAO.getGeoInfo();
 		
-		
+		req.setAttribute("geoList", geoList);
 		req.setAttribute("main_jsp", "../maemool/list.jsp");
 		return "main.jsp";
 	}
